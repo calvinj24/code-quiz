fb47b131548f8dd652b7c36ee58eca22f264e2b0
Calvin Johnson <calvinjohnson@Calvins-MBP-7.attlocal.net>
2020-09-25 22:03:52 -0500

    add question choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,70 @@
+var pageContent = document.querySelector(".page-content");
 // Question Array
 var questions = [
-    {q:"This is a question?", c: ["choice1", "choice2", "choice3", "choice4"], a:1},
-    {q:"This is a question?", c: ["choice1", "choice2", "choice3", "choice4"], a:1},
-    {q:"This is a question?", c: ["choice1", "choice2", "choice3", "choice4"], a:1},
-    {q:"This is a question?", c: ["choice1", "choice2", "choice3", "choice4"], a:1},
-    {q:"This is a question?", c: ["choice1", "choice2", "choice3", "choice4"], a:1}
+    {q: "This is a question?", c: ["choice1", "choice2", "choice3", "choice4"], a:"1"},
+    {q: "This is a question?", c: ["choice1", "choice2", "choice3", "choice4"], a:"1"},
+    {q: "This is a question?", c: ["choice1", "choice2", "choice3", "choice4"], a:"1"},
+    {q: "This is a question?", c: ["choice1", "choice2", "choice3", "choice4"], a:"1"},
+    {q: "This is a question?", c: ["choice1", "choice2", "choice3", "choice4"], a:"1"}
 ];
 var questionEl = document.querySelector("#question");
 var choiceListEl = document.querySelector("#choice-list");
 var questionNumber = 0;
+var currentQuestion = "";
+var answer="";
+var choiceContainer="";
 
-var newQuestion = function() {
-    var currentQuestion=questions[questionNumber];
+
+
+
+var startQuiz = function() {
+    for (i=0; i< questions.length; i++) {
+        newQuestion();
+    }
+};
+
+var newQuestion = function(questionNumber) {
+    currentQuestion=questions[questionNumber];
+    answer=currentQuestion.a;
+    var choiceList = currentQuestion.c;
     
     // mew question
     questionEl.textContent=currentQuestion.q;
 
     // new choices
-    for (i=0; i<currentQuestion.c.length; i++) {
-        var choiceEl = document.createElement("li");
-        choiceEl.className = "choice";
-        choiceEl.textContent = currentQuestion.c[i];
-        choiceListEl.appendChild(choiceEl);
-    }
+    choiceContainer = document.createElement("div");
+    choiceContainer.className = "choice-container";
+    pageContent.appendChild(choiceContainer);
+
+    for (i=0; i<choiceList.length; i++) {
+        newChoice(i);
+    };
+};
+
+var newChoice = function (choiceNumber) {
+    var choiceEl = document.createElement("button");
+    choiceEl.textContent = (choiceNumber+1) + ". " + currentQuestion.c[choiceNumber];
+    choiceEl.className = "choice";
+    choiceEl.id="choice" + (choiceNumber + 1);
+    choiceContainer.appendChild(choiceEl);
+    choiceEl.addEventListener("click",pickChoice);
 }
 
-newQuestion()
+var pickChoice = function() {
+    pickedChoice = event.target;
+    pickedChoiceID = pickedChoice.id
+
+    if (pickedChoiceID[pickedChoiceID.length-1] === answer) {
+        console.log("Right");
+    } else {
+        console.log("Wrong");
+    };
+};
+// event listeners
+
+// for loop
+var startQuiz  
+
+newQuestion(0) 
+
+
